0d3b0bb677ec8a6ae61a23fd481ec0c99f2b9c6a
kawada <vgnsl94@gmail.com>
2021-03-24 16:11:34 +0900

    s

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
+
 require('./bootstrap');
+
+import './bootstrap'
+import Vue from 'vue';
+import Vuetify from './plugins/vuetify' // vuetify.jsを読み込み
+
+import ExampleComponent from './components/ExampleComponent.vue'
+
+const app = new Vue({
+    el: '#app',
+    vuetify: Vuetify,
+    components: {
+        ExampleComponent,
+    }
+});
