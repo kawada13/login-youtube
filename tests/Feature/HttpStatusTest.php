8a4073583b6e10f0f20ba0b11895622c13c5c5d6
kawada <vgnsl94@gmail.com>
2021-03-30 16:15:41 +0900

    s

---
+++ tests/Feature/HttpStatusTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Support\Str;
+
+class HttpStatusTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+
+     public function testIndexStatus() 
+     {
+        $response = $this->post('/api/category', [
+            'name'  => 'c',
+            'slug'  => 'fd',
+        ]);
+        $response->assertStatus(200);
+     }
+}
+
+
