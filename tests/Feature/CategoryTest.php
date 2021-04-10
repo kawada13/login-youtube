cc1653345367ea119e158bea6a8b98bb9589eecd
kawada <vgnsl94@gmail.com>
2021-04-10 13:31:52 +0900

    editテスト作成

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -15,7 +15,7 @@ class CategoryTest extends TestCase
      *
      * @return void
      */
-    public function test_新しい投稿を作成して返却する()
+    public function test_store()
     {
 
         $data = [
@@ -34,12 +34,29 @@ class CategoryTest extends TestCase
 
     }
 
-    public function test_正しい構造のjsonを返却する()
+    public function test_index()
     {
         $response = $this->json('GET', route('category.index'));
 
         $response
             ->assertStatus(200)
             ->assertJson(['message' => '成功']);
+    }
+
+    public function test_edit()
+    {
+        factory(Category::class)->create();
+        $category = Category::first();
+
+        $response = $this->json('GET', route('category.edit', [
+            'category' => $category->id,
+        ]));
+
+        $response->assertStatus(200)
+            ->assertJsonFragment([
+                'id' => $category->id,
+                'name' => $category->name,
+                'slug' => $category->slug,
+            ]);
     }
 }
